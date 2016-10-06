8ce476eae97d78f2fd5ab2ca619c1370215623a1
Desi Desario <desireedesario@gmail.com>
2016-10-05 22:27:40 -0700

    responsive for mobile device

--- services/chat.dataservice.js
+++ services/chat.dataservice.js
@@ -15,7 +15,7 @@
     // vm.addChat = addChat;
     // vm.deleteChat = deleteChat;
     // vm.updateChat = updateChat;
-
+    //
     // //This is where I would place call backs to the back end. //$http should be injected and used
     //   //GET all chat groups
     //   function getAllChats(){
@@ -56,79 +56,11 @@
 
     //hardcoded seeds
     vm.all = [
-      {chatName: "Sammy and Tina", content: "Hey How are you?", participants: ["Conrad", "Andrew"]},
+      {chatName: "The Three Musketeers", content: "This is Conrad, WHATS GOING ON?", participants: ["Conrad", "Andrew"]},
       {chatName: "Spanish Class 101", content: "Are you ready for the test?", participants: ["Mike", "Kate", "Ira"]},
       {chatName: "Mario Party Party", content: "The party is going to be this Friday.", participants: ["Brittany", "Storm", "Joshua"]},
       {chatName: "Mom and Dad", content: "How are you son?", participants: ["Mom", "Dad"]}
     ]
   }
-
-  // back end calls
-  //   GET (get all chats)
-  //   function getAllChats(res, req){
-  //     Chats.find(function(err, chats) {
-  //       if(err) res.json({msg:"Could not find any chat groups"});
-  //
-  //       res.json({chats: chats});
-  //     });
-  //   }
-  //   // POST (create a new chat)
-  //   function createChat(res, req){
-  //     console.log("body: ", req.body);
-  //
-  //     var chat = new Chat(req.body);
-  //
-  //     chat.save(function(err) {
-  //       if(err) res.json({msg:"Could  not create this chat group because: " + err});
-  //
-  //       res.json({chat: chat});
-  //     });
-  //   }
-  //   // GET (show a single chat)
-  //   function getChat(req, res){
-  //     var id = req.params.id;
-  //
-  //     Chat.findById({_id: id}, function(err, chat){
-  //       if(err) res.json({msg:"Could not find chat group because: " + err});
-  //
-  //       res.json({chat: chat});
-  //     })
-  //   }
-  //   //PUT (update a chat)
-  //   function updateChat(req, res){
-  //     var id = req.params.id;
-  //
-  //     Chat.findById({_id: id}, function(err, chat){
-  //       if(err) res.json({msg:"Could not find this chat group because: " + err});
-  //
-  //       if(req.body.chatName) chat.chatName = req.body.chatName;
-  //       if(req.body.content) chat.content = req.body.content;
-  //       if(req.body.participants) chat.participants = req.body.participants;
-  //
-  //       chat.save(function(err){
-  //         if(err) res.json({msg: "Could not update this chat group because: " + err});
-  //
-  //         res.json({msg: "Chat group has sucessfully been updated", chat: chat})
-  //       });
-  //     });
-  //   }
-  //   // DELETE (remove a chat board)
-  //   function removeChat(res, req){
-  //     var id = req.params.id;
-  //
-  //     Chat.remove({_id: id}, function(err){
-  //       if(err) res.json({msg: "Could not delete this chat group because", err});
-  //
-  //       res.json({msg: "chat group sucessfully deleted"});
-  //     });
-  //   }
-  //
-  //   module.exports = {
-  //     getAllChats  = getAll,
-  //     createChat   = createChat,
-  //     getChat      = getChat,
-  //     updateChat   = updateChat,
-  //     removeChat   = removeChat
-  //   }
 
 })();
